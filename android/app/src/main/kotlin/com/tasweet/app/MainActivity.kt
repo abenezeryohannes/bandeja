02d1b86efbe83fcba2eb0fef7a6f3074315d8f5f
Abenezer <abenezeryohannes@gmail.com>
2022-10-14 10:11:36 +0300

    preparing for bandejs

--- android/app/src/main/kotlin/com/tasweet/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tasweet.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/core/fixtures/fixture.reader.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:flutter/services.dart' show rootBundle;
-
-String fixture(String name) => File("assets/fixtures/$name").readAsStringSync();
-
-Future<String> fixturesFromAsset(String name) async =>
-    rootBundle.loadString('assets/fixtures/$name');
