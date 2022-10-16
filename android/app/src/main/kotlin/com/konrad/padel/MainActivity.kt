6e6d4223811efeb9e868c0be904a6d79952c4ab1
Abenezer <abenezeryohannes@gmail.com>
2022-10-16 11:39:17 +0300

    'fix'

--- android/app/src/main/kotlin/com/konrad/padel/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.konrad.padel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main/main.admin.dart
+++
@@ -1,7 +0,0 @@
-import 'package:feekpadel/main/main.common.dart';
-import '../src/flavors.dart';
-
-void main() {
-  FF.appFlavor = Flavor.admin;
-  mainCommon();
-}
